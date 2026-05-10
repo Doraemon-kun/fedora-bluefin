5666e24acf38c3f21def40eceef31fa8135267cf
AM <58893941+Doraemon-kun@users.noreply.github.com>
2026-05-10 20:19:01 +0700

    fix+feat: Add extensions and potential fix for gschemas (#61)
    - Add Background Logo extension from Fedora
    - Enable some small dconf settings in Bluefin
    - Disable dconf update after remove
    - Modify Anaconda to create user account after first boot

--- build/20-de-bluefinify.sh
+++ build/20-de-bluefinify.sh
@@ -27,16 +27,50 @@ dnf5 -y reinstall plymouth-theme-spinner
 
 # Removing GNOME gschema from Bluefin and regenerate the gschemas
 rm -f /usr/share/glib-2.0/schemas/zz0-bluefin-modifications.gschema.override
-cat <<EOF > /usr/share/glib-2.0/schemas/98-window-buttons.gschema.override
-[org.gnome.desktop.wm.preferences]
-button-layout=':minimize,maximize,close'
-EOF
-cat <<EOF > /usr/share/glib-2.0/schemas/99-custom-extensions.gschema.override
+cat <<EOF > /usr/share/glib-2.0/schemas/99-custom-settings.gschema.override
 [org.gnome.shell]
 disable-extension-version-validation=true
 enabled-extensions=['AlphabeticalAppGrid@stuarthayhurst', 'freon@UshakovVasilii_Github.yahoo.com', 'status-area-horizontal-spacing@mathematical.coffee.gmail.com', 'appindicatorsupport@rgcjonas.gmail.com', 'blur-my-shell@aunetx', 'caffeine@patapon.info', 'gsconnect@andyholmes.github.io', 'netspeed@alynx.one', 'just-perfection-desktop@just-perfection', 'simple-weather@romanlefler.com']
+
+[org.gnome.desktop.wm.preferences]
+button-layout=':minimize,maximize,close'
+
+[org.gnome.desktop.interface]
+enable-hot-corners=true
+clock-show-weekday=true
+
+[org.gnome.desktop.search-providers]
+enabled=['io.github.kolunmi.Bazaar.desktop', 'org.gnome.Calculator.desktop']
+
+[org.gnome.desktop.sound]
+allow-volume-above-100-percent=true
+
+[org.gnome.desktop.wm.keybindings]
+show-desktop=['<Super>d']
+switch-applications=['<Super>Tab']
+switch-applications-backward=['<Shift><Super>Tab']
+switch-windows=['<Alt>Tab']
+switch-windows-backward=['<Shift><Alt>Tab']
+switch-input-source=['<Super>space']
+switch-input-source-backward=['']
+unmaximize=['<Super>Down']
+
+[org.gnome.desktop.peripherals.keyboard]
+numlock-state=true
+
+[org.gnome.settings-daemon.plugins.power]
+power-button-action='interactive'
+
+[org.gtk.Settings.FileChooser]
+sort-directories-first=true
+
+[org.gtk.gtk4.Settings.FileChooser]
+sort-directories-first=true
+
+[org.gnome.mutter]
+center-new-windows=true
 EOF
-glib-compile-schemas /usr/share/glib-2.0/schemas
+glib-compile-schemas /usr/share/glib-2.0/schemas/
 
 # Remove Dconf database files
 rm -f /etc/dconf/db/distro.d/01-bluefin-folders
@@ -46,7 +80,7 @@ rm -f /etc/dconf/db/distro.d/04-bluefin-custom-command-menu
 rm -f /etc/dconf/db/distro.d/05-bluefin-searchlight-extension
 rm -f /etc/dconf/db/distro.d/locks/01-bluefin-locked-settings
 # And update it back
-dconf update
+#dconf update
 
 # Remove Terminal stuff at profile (i.e. stop running when open terminal)
 rm -f /etc/profile.d/ublue-motd.sh
